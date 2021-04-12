927adee989d8597acb02521354e616aa3d0f4346
Earther <qn.khuat@gmail.com>
2021-04-12 18:45:25 +0700

    update install.sh

--- data/install.sh
+++ data/install.sh
@@ -1,4 +1,4 @@
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /user/local/bin/youtube-dl
 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
