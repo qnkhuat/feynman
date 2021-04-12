f04c7af16ec4005fe14281c9fc9c77811cfc6432
Earther <qn.khuat@gmail.com>
2021-04-12 18:31:44 +0700

    remove download dataset at start.sh

--- TTS/start.sh
+++ TTS/start.sh
@@ -3,11 +3,11 @@ git config --global user.name "Ngoc Khuat"
 apt install vim
 curl https://raw.githubusercontent.com/qnkhuat/scripts/master/.vimrc -o ~/.vimrc
 # Load datset
-mkdir datasets
-echo "Download dataset"
-curl https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2 -o datasets/LJSpeech-1.1.tar.bz2
-cd datasets/
-tar -xf LJSpeech-1.1.tar.bz2
+#mkdir datasets
+#echo "Download dataset"
+#curl https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2 -o datasets/LJSpeech-1.1.tar.bz2
+#cd datasets/
+#tar -xf LJSpeech-1.1.tar.bz2
 
 # Install dependencies
 apt-get install espeak-ng
