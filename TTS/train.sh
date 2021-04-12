32a26da6c31e2799e4604b188b9c04dfa8f96dfb
Earther <qn.khuat@gmail.com>
2021-04-12 19:27:37 +0700

    add train

--- TTS/train.sh
+++ TTS/train.sh
@@ -1,1 +1,3 @@
-CUDA_VISIBLE_DEVICES="0" python3 TTS/bin/train_tacotron.py --config_path ./config.json
+gdown https://drive.google.com/uc?id=1CFoPDQBnhfBFu2Gc0TBSJn8o-TuNKQn7 -O tacotron2.pth.tar
+CUDA_VISIBLE_DEVICES="0" python3 TTS/bin/train_tacotron.py --config_path ./config.json --restore_path tacotron2.pth.tar
+
